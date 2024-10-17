58189f28be8d2df8ce93d9b73eb34c69bdbc5efd
gmahota <guimaraesmahota@gmail.com>
2024-10-17 10:09:45 +0200

    update from typeorm to prisma

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,26 +1,49 @@
 import { Router, Request, Response } from "express";
-import authMiddleware from "../middlewares/auth";
+import { v4 } from "uuid";
+
+// auth routes
+import authRouter from "./auth/auth";
+import loginRouter from "./auth/login";
+import registerRouter from "./auth/register";
 
 // System Routers
-import adminRouter from "./admin";
-import authRouter from "./auth";
 import baseRouter from "./base";
+
 
 const routes = Router();
 
+routes.get("/api", (request: Request, response: Response) => {
+  const path = `/api/item/${v4()}`;
+  response.setHeader("Content-Type", "text/html");
+  response.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
+  response.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
+
+routes.get("/api/item/:slug", (request: Request, response: Response) => {
+  const { slug } = request.params;
+  response.end(`Item: ${slug}`);
+});
 /**
  * @swagger
  * /:
  *   get:
  *     summary: Home Page
  *     description: Can be used to testing an API.
-*/
+ */
 routes.get("/", async (request: Request, response: Response) => {
   response.send("WellCome!");
 });
 
-routes.use('/api/admin',adminRouter);
-routes.use('/api/auth',authRouter);
-routes.use('/api/base',baseRouter);
+routes.get("/ping", (_req: Request, res: Response) => {
+  return res.send("pong 🏓");
+});
+
+// Auth Routes
+routes.use("/api/auth", authRouter);
+routes.use("/api/auth", loginRouter);
+routes.use("/api/auth", registerRouter);
+
+// Aplication Base
+routes.use("/api/base", baseRouter);
 
 export default routes;
